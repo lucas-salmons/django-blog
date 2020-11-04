d108d43666fcca5ea4deead130d10772adf3594f
Lucas Salmons <lucsalmons@gmail.com>
2020-11-03 21:05:34 -0800

    updated blogging admin.py to add inline categories and restricted tagging posts from the categoryAdmin

--- blogging/admin.py
+++ blogging/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 from blogging.models import Post, Category
 # Register your models here.
 
-admin.site.register(Post)
-admin.site.register(Category)
+class CatergoriesInline(admin.TabularInline):
+    model = Category.posts.through
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CatergoriesInline,
+    ]
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category,CategoryAdmin)
