02cff1ca08e2b8eb71f4ed562cc4c400fec72748
Lucas Salmons <lucsalmons@gmail.com>
2020-11-16 22:33:15 -0800

    Applied black

--- blogging/tests.py
+++ blogging/tests.py
@@ -3,48 +3,54 @@ from django.utils.timezone import utc
 from django.test import TestCase
 from django.contrib.auth.models import User
 from blogging.models import Post, Category
+
 # Create your tests here.
 
+
 class PostTestCase(TestCase):
-    fixtures = ['blogging_test_fixture.json', ]
+    fixtures = [
+        "blogging_test_fixture.json",
+    ]
 
     def setUp(self):
         self.user = User.objects.get(pk=1)
-    
+
     def test_string_representation(self):
         expected = "This is a title"
         p1 = Post(title=expected)
         actual = str(p1)
         self.assertEqual(expected, actual)
 
-class CategoryTestCase(TestCase):
 
+class CategoryTestCase(TestCase):
     def test_string_representation(self):
         expected = "A Category"
         c1 = Category(name=expected)
         actual = str(c1)
         self.assertEqual(expected, actual)
 
+
 class FrontEndTestCase(TestCase):
     """test views provided in the front-end"""
-    fixtures = ['blogging_test_fixture.json', ]
+
+    fixtures = [
+        "blogging_test_fixture.json",
+    ]
 
     def setUp(self):
         self.now = datetime.datetime.utcnow().replace(tzinfo=utc)
         self.timedelta = datetime.timedelta(15)
         author = User.objects.get(pk=1)
         for count in range(1, 11):
-            post = Post(title="Post %d Title" % count,
-                        text="foo",
-                        author=author)
+            post = Post(title="Post %d Title" % count, text="foo", author=author)
             if count < 6:
                 # publish the first five posts
                 pubdate = self.now - self.timedelta * count
                 post.published_date = pubdate
             post.save()
-    
+
     def test_list_only_published(self):
-        resp = self.client.get('/')
+        resp = self.client.get("/")
         # the content of the rendered response is always a bytestring
         resp_text = resp.content.decode(resp.charset)
         self.assertTrue("Spicy Blog Posts" in resp_text)
@@ -59,7 +65,7 @@ class FrontEndTestCase(TestCase):
         for count in range(1, 11):
             title = "Post %d Title" % count
             post = Post.objects.get(title=title)
-            resp = self.client.get('/posts/%d/' % post.pk)
+            resp = self.client.get("/posts/%d/" % post.pk)
             if count < 6:
                 self.assertEqual(resp.status_code, 200)
                 self.assertContains(resp, title)
