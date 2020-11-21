4fc9cc3b11fb6cfa8a3ff4bd47e505e953ca9ada
Michael List <michael.list.work@gmail.com>
2020-11-21 12:04:06 -0800

    removed and added files for rest api

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from blogging.views import BlogListView, BlogDetailView, add_model
+from blogging.views import BlogListView, BlogDetailView
 
 urlpatterns = [
     path("", BlogListView.as_view(), name="blog_index"),
-    path("add/", add_model, name="add_post"),
     path("posts/<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
 ]
