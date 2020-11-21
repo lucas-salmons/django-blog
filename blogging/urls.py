20ccd5d45e738cf13f8938220a3491b4a70f95cb
Michael List <michael.list.work@gmail.com>
2020-11-21 14:52:55 -0800

    moved urls for api from blogging to mysite

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,21 +1,7 @@
 from django.urls import include, path
-from rest_framework import routers
-from blogging.views import (
-    BlogListView,
-    BlogDetailView,
-    UserViewSet,
-    CategoryViewSet,
-    PostViewSet,
-)
-
-router = routers.DefaultRouter()
-router.register(r"users", UserViewSet)
-router.register(r"categories", CategoryViewSet)
-router.register(r"posts", PostViewSet)
+from blogging.views import BlogListView, BlogDetailView
 
 urlpatterns = [
     path("", BlogListView.as_view(), name="blog_index"),
     path("posts/<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
-    path("api/", include(router.urls)),
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-]
+    ]
