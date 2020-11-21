4c1f0de5ef1729a39d85f610d42b07aea0c4f5d3
Michael List <michael.list.work@gmail.com>
2020-11-21 13:13:13 -0800

    completed urls and views for rest api

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
-from blogging.views import BlogListView, BlogDetailView
+from django.urls import include, path
+from rest_framework import routers 
+from blogging.views import BlogListView, BlogDetailView, UserViewSet, CategoryViewSet, PostViewSet
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'categories', CategoryViewSet)
+router.register(r'posts', PostViewSet)
 
 urlpatterns = [
     path("", BlogListView.as_view(), name="blog_index"),
     path("posts/<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
