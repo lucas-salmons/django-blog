2d27a7e89df154f1ed004df6873c95c58611e24a
Michael List <michael.list.work@gmail.com>
2020-11-21 14:55:18 -0800

    ran black

--- blogging/urls.py
+++ blogging/urls.py
@@ -4,4 +4,4 @@ from blogging.views import BlogListView, BlogDetailView
 urlpatterns = [
     path("", BlogListView.as_view(), name="blog_index"),
     path("posts/<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
-    ]
+]
