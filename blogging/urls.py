f1fe0ce90c332b3795e50b3c8564dda49763a0ed
Michael List <michael.list.work@gmail.com>
2020-11-21 11:55:26 -0800

    added add_model

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from blogging.views import BlogListView, BlogDetailView
+from blogging.views import BlogListView, BlogDetailView, add_model
 
 urlpatterns = [
     path("", BlogListView.as_view(), name="blog_index"),
+    path("add/", add_model, name="add_post"),
     path("posts/<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
 ]
